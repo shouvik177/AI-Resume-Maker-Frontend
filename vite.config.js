495d84f0b23e466fa9c9132911cff4bd019f957a
Shouvik <shouvikmazumdar177@gmail.com>
2025-04-04 03:02:08 +0530

    config file changes

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,21 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   build: {
-    outDir: 'dist',
+    outDir: "dist",
   },
   server: {
-    historyApiFallback: true,
+    historyApiFallback: true, // Ensures local routing works
+  },
+  preview: {
+    historyApiFallback: true, // Ensures preview mode also works
   }
 });
