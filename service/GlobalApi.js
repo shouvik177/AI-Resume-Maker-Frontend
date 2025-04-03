f19eab87c54d48ba994c56a888316675c53125d0
Shouvik <shouvikmazumdar177@gmail.com>
2025-04-04 04:46:57 +0530

    Global API issue fixed

--- service/GlobalApi.js
+++ service/GlobalApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://shk-vts.onrender.com"; 
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL 
 const API_KEY = import.meta.env.VITE_STRAPI_API_KEY;
 
 const axiosClient = axios.create({
